c7c8474250b7a618a1caf88275e83fcd2d488f24
Oskar Blyt <oskariblyt@gmail.com>
2021-11-07 23:08:53 -0800

    updates work_report and comments

--- clusters/selection.py
+++ clusters/selection.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# sorts from low to high and removes the percent% highest energy Clusters from the list of Clusters
 def remove_top_percent(clusters, percent):
     size = len(clusters)
     count = int(percent * size / 100)
