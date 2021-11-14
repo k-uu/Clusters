524a0b8fa7e94e4f88d07fcbb99e9ce9d3ab0fe4
Oskar Blyt <oskariblyt@gmail.com>
2021-11-14 14:35:44 -0800

    adds ImmigrateOperator and TwistOperator

--- clusters/management.py
+++ clusters/management.py
@@ -18,9 +18,8 @@ class Operator:
 # center
 class AngularOperator(Operator):
     def apply(self, clusters, size):
-        target_count = int(size * self.rate)
         rng = np.random.default_rng()
-        targets = rng.choice(clusters, target_count, replace=False)
+        targets = get_targets(self, clusters, size)
         percent = 0.05
         for t in targets:
             index_p = rng.choice(np.arange(0, t.size), int(math.ceil(t.size * percent)), replace=False)
@@ -38,8 +37,44 @@ class AngularOperator(Operator):
         return targets
 
 
+# produces new individuals in the same manner the initial population was created
+class ImmigrateOperator(Operator):
+    def apply(self, clusters, size):
+        particles = clusters[0].size
+        target_count = int(size * self.rate)
+        return initialize.make_population(target_count, particles)
+
+
+# produces individuals by rotating half the cluster about the x-axis by a random amount
+class TwistOperator(Operator):
+    def apply(self, clusters, size):
+        rng = np.random.default_rng()
+        targets = get_targets(self, clusters, size)
+        index_p = len(targets[0].pos)
+        for t in targets:
+            rot = rng.uniform(0.1, 0.5) * np.pi
+            cos = np.cos(rot)
+            sin = np.sin(rot)
+            rot_x_mat = np.array([[1, 0, 0],
+                                  [0, cos, -sin],
+                                  [0, sin, cos]])
+            for p in range(index_p):
+                if np.dot(t.pos[p], [1, 0, 0]) >= 0:
+                    t.pos[p] = np.matmul(rot_x_mat, t.pos[p])
+        return targets
+
+
 def mean_energy(clusters):
     return sum(clusters) / len(clusters)
+
+
+# returns a random selection from clusters based on the operator creation rate and
+# final size of new population
+def get_targets(op, clusters, size):
+    target_count = int(size * op.rate)
+    rng = np.random.default_rng()
+    targets = rng.choice(clusters, target_count, replace=False)
+    return targets
 
 
 # piece-wise function that determines the variance to a operator creation rate based off of previous average energy
@@ -76,11 +111,12 @@ def normalize(operators):
     return
 
 
-# op = AngularOperator(0.5)
+# op = TwistOperator(0.5)
 # pop = initialize.make_population(2, 5)
 # print(pop[0].pos)
 # print(pop[1].pos)
 # print(res := op.apply(pop, 2))
+# print("After")
 # print(res[0].pos)
 
 
